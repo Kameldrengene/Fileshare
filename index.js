a7140e5c03c5254f7b49b0c07c4b161f56959707
Mark <s174881@student.dtu.dk>
2021-02-26 11:59:24 +0100

    Async kald til MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 var http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    mongo = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
+
+var url = "mongodb://130.225.170.68:27017/";
+
+function getUsersAsync() {
+    return new Promise(resolve => {
+        //resolve(getUsers());
+        resolve('Test');
+    });
+}
+function getUsers() {
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) return console.log(err);
+        console.log("Database Connected!");
+        var dbo = db.db("Login");
+        dbo.collection("User").find({}).toArray(function (err, result) {
+            if (err) return console.log(err);
+            console.log(result);
+        });
+        db.close();
+        return result;
+    });
+}
 
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8081);
+const server = http.createServer();
+server.on('request', async (req, res) => {
+    const data = await getUsersAsync();
+    console.log(data);
+    res.end(data);
 });
+server.listen(8081);
+
+
