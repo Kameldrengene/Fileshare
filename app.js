5afe57989f5730c2dddd63178bbfe74369966197
Volkan Isik <s180103@student.dtu.dk>
2021-04-30 10:56:01 +0200

    cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var db = require('./db');
 var AuthController = require('./auth/AuthController');
 var Fileupload = require('./fileupload');
 var Users = require('./users');
-app.options('*',cors());
+app.use(cors());
 app.use('/api/auth', AuthController);
 app.use('/api/files', Fileupload);
 app.use('/api/user', Users);
